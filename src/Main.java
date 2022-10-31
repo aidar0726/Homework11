7318b3ad64e1f5f200e069aafac3df576a36cff4
shakirov_a <Shakirov0726@gmail.com>
2022-10-31 12:08:16 +0600

    исправленная версия ДЗ 11(методы)

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,42 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("//Задание 1");
-        determinesLeapYear (2004);
+        determinesLeapYear(2004);
         System.out.println("//Задание 2");
-        indicatesNeedUpdates (1,2012);
+        indicatesNeedUpdates(1, 2012);
         System.out.println("//Задание 3");
         System.out.println("Потребуется дней: " + returnNumberDeliveryDays(30));
 
     }
 
-    public static void determinesLeapYear ( int year){
-        if ((year % 4) == 0 && (year % 100 != 0 && year % 400 == 0)) {
+    public static void determinesLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println("Високосный год");
         } else {
             System.out.println("Невисокосный год");
         }
     }
 
-    public static void indicatesNeedUpdates (int clientOS, int clientDeviceYear) {
+    public static void indicatesNeedUpdates(int clientOS, int clientDeviceYear) {
 
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear >= 2015 ) {
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else if (clientOS == 0 && clientDeviceYear <= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке,также необходимо установить облегченную версию приложения");
-        } else if (clientOS == 1 && clientDeviceYear <= 2015 ) {
+        } else if (clientOS == 1 && clientDeviceYear <= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке,также необходимо установить облегченную версию приложения");
         }
     }
 
-    public static int returnNumberDeliveryDays (int distance) {
+    public static int returnNumberDeliveryDays(int distance) {
 
-        if (distance <=  20) {
+        if (distance <= 20) {
             return 1;
         } else if (distance > 20 && distance < 60) {
             return 2;
-        } else  {
+        } else {
             return 3;
         }
     }
